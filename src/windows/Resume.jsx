c5e748c400b4e82d1b024eba5d8b60b20c878e7a
Jorge Anthony Sanchez Chavez <jorgesanchez99dev@gmail.com>
2025-12-04 16:19:04 -0500

    add metadata

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -6,10 +6,7 @@ import {pdfjs, Document, Page} from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const Resume = () => {
   const fileUrl = "files/CV_Jorge_Sanchez.pdf";
